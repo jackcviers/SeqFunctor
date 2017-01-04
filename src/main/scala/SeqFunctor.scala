0c5f0b4a17c2301164ce9762d4a5f0b174fd9693
Jack Viers <jackcviers@gmail.com>
2017-01-04 10:58:48 -0600

    Fix wrong paste

--- src/main/scala/SeqFunctor.scala
+++ src/main/scala/SeqFunctor.scala
@@ -1,19 +1,16 @@
 package io.github.jackcviers.functorInstances
 
 import cats.Functor
-import org.scalatest._
-import org.typelevel.discipline.scalatest.Discipline
-import cats.instances.all._
-import cats.laws.discipline.arbitrary._
-import cats.laws.discipline.FunctorTests
-import SeqFunctorInstances._
+import cats.kernel.Eq
 
-class SeqFunctorInstancesSpec extends FunSuite with Discipline with Matchers{
-  checkAll("Functor[Seq[Int]]", FunctorTests[Seq].functor[Int, Int, Int])
 
-  test("should be useable in a generic function"){
-    def usesSeqFunctor(g: Seq[Char])(implicit fSeq: Functor[Seq]): Seq[Int] = fSeq.map(g) { _.toInt}
+object SeqFunctorInstances{
 
-    usesSeqFunctor("Hayooo") shouldBe Seq(72, 97, 121, 111, 111, 111)
+  implicit def seqFunctor = new Functor[Seq]{
+    def map[A, B](fa: Seq[A])(f: A => B):Seq[B] = fa.map(f)
+  }
+
+  implicit def seqEq[A: Eq] = new Eq[Seq[A]]{
+    def eqv(x: Seq[A], y:Seq[A]) = x == y
   }
 }
